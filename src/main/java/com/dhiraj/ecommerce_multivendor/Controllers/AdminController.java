b2cef5755aea3c228a5f56a32fe7bee14b558bc1
Dhiraj <dk194945@gmail.com>
2025-01-16 18:34:10 +0530

    Add HomeCategory retrieval and update endpoints in AdminController

--- src/main/java/com/dhiraj/ecommerce_multivendor/Controllers/AdminController.java
+++ src/main/java/com/dhiraj/ecommerce_multivendor/Controllers/AdminController.java
@@ -1,7 +1,12 @@
 package com.dhiraj.ecommerce_multivendor.Controllers;
 
+import java.util.List;
+import com.dhiraj.ecommerce_multivendor.Modals.HomeCategory;
+import com.dhiraj.ecommerce_multivendor.Service.HomeCategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +23,33 @@ import lombok.RequiredArgsConstructor;
 public class AdminController {
 
     private final SellerService sellerService;
+    private final HomeCategoryService homeCategoryService;
 
     @PatchMapping("/seller/{id}/status/{status}")
-    public ResponseEntity<Seller> updateSellersStatus(@PathVariable Long id, @PathVariable AccountStatus status)
+    public ResponseEntity<Seller> updateSellersStatus(@PathVariable Long id,
+            @PathVariable AccountStatus status)
             throws Exception {
 
         Seller updateSeller = sellerService.updateSellerStatus(id, status);
         return ResponseEntity.ok(updateSeller);
+
+    }
+
+    @GetMapping("/home-category")
+    public ResponseEntity<List<HomeCategory>> getHomeCategory() throws Exception {
+
+        List<HomeCategory> categories = homeCategoryService.getAllHomeCategories();
+        return ResponseEntity.ok(categories);
+
+    }
+
+    @PatchMapping("/home-category/{id}")
+    public ResponseEntity<HomeCategory> updateHomeCategory(
+            @PathVariable Long id,
+            @RequestBody HomeCategory homeCategory) throws Exception {
+
+        HomeCategory updatedCategory = homeCategoryService.updateHomeCategory(homeCategory, id);
+        return ResponseEntity.ok(updatedCategory);
 
     }
 }
